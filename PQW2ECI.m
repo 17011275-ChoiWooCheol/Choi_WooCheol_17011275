74353c2dec7bdd7b8cd0ff034659b7a245f620ad
bomfe <bomfe@BOOK-DUQ723QA4L>
2023-05-22 18:02:43 +0900

    For Week#11 HW 17011275 최우철

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -1,10 +1,10 @@
 function R_pqw_eci = PQW2ECI(arg_prg, inc_angle, RAAN)
-    % Convert angles to radians
+    %-degree to radian
     arg_prg = deg2rad(arg_prg);
     inc_angle = deg2rad(inc_angle);
     RAAN = deg2rad(RAAN);
 
-    % Calculate rotation matrix elements
+    % Each elements of rotation matrix 
     cos_arg_prg = cos(arg_prg);
     sin_arg_prg = sin(arg_prg);
     cos_inc_angle = cos(inc_angle);
@@ -12,7 +12,7 @@ function R_pqw_eci = PQW2ECI(arg_prg, inc_angle, RAAN)
     cos_RAAN = cos(RAAN);
     sin_RAAN = sin(RAAN);
 
-    % Construct the rotation matrix
+    % Calculatioh of rotation matrix
     R_pqw_eci = [-sin_RAAN*cos_inc_angle*sin_arg_prg + cos_RAAN*cos_arg_prg, ...
                  -sin_RAAN*cos_inc_angle*cos_arg_prg - cos_RAAN*sin_arg_prg, ...
                  sin_RAAN*sin_inc_angle;
